b7bf0f235c1ff3f45f922383403c2c16fa224d32
Rebbouh-Mohamed <info.moh.2003@gmail.com>
2025-03-19 23:07:54 +0100

    Update transfer.py

--- transfer.py
+++ transfer.py
@@ -8,7 +8,7 @@ def transfe(content_images,style_images,progress):
     optimization_config['content_weight']=1e5    
     optimization_config['style_weight']=3e4 
     optimization_config['tv_weight']=1e0
-    optimization_config['height']=400
+    optimization_config['height']=1000
     optimization_config['content_images_dir'] = content_images#'./dancing.jpg'
     optimization_config['style_images_dir'] = style_images#'./vane.jpg'
     # optimization_config['output_img_dir'] = output_img_dir
